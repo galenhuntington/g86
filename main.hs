6aa949303301e12cac2347cf32bf93b4b03b1bf2
Galen Huntington <galen@alumni.reed.edu>
2019-02-27 01:39:10 -0800

    Chunk encoded output.

--- main.hs
+++ main.hs
@@ -2,11 +2,12 @@ import G86
 import System.IO
 import System.Environment
 import qualified Data.ByteString as B
+import Data.List.Split (chunksOf)
 
 main = do
    args <- getArgs
    case args of
-      [] -> putStrLn =<< encode <$> B.getContents
+      [] -> putStr =<< unlines . chunksOf 80 . encode <$> B.getContents
       ["-d"] -> B.putStr =<< decode <$> getContents
       _ -> hPutStrLn stderr "Usage: g86 [-d] < input > output"
 
